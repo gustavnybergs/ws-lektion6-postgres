e3fcc5f70a0fb74f8cfd4c4859aa7296b71412c3
gustavnybergs <Gustavnybergs@gmail.com>
2025-09-03 22:28:15 +0200

    lektion6: kopplad till Postgres, POST/GET funkar

--- src/main/java/com/gustav/ws_lektion6_postgres/WsLektion6PostgresApplication.java
+++ src/main/java/com/gustav/ws_lektion6_postgres/WsLektion6PostgresApplication.java
@@ -2,12 +2,14 @@ package com.gustav.ws_lektion6_postgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.gustav.ws")
+@EnableJpaRepositories("com.gustav.ws.repo")
+@EntityScan("com.gustav.ws.model")
 public class WsLektion6PostgresApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WsLektion6PostgresApplication.class, args);
 	}
-
 }
